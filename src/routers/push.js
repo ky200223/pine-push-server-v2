f8c73758c9d013859b0318864bd20e3db2592855
reaperes <emerald105@hanmail.net>
2014-10-07 12:29:10 +0900

    update rabbit mq message

--- src/routers/push.js
+++ src/routers/push.js
@@ -2,8 +2,6 @@ var info = require('debug')('info:routers:push');
 var router = require('express').Router();
 var rabbitmq = require(SOURCE_ROOT + '/modules/rabbitmq/rabbitmq.js');
 
-// todo /push/register
-
 /**
  * @module routers/push
  * @description /push router
@@ -20,6 +18,14 @@ var rabbitmq = require(SOURCE_ROOT + '/modules/rabbitmq/rabbitmq.js');
  *   device_name:  (required, String),
  *   device_type:  (required, String - ios, android),
  *   device_id:    (required, String, gcm reg_id or apns token)
+ * }
+ *
+ * RabbitMQ Request
+ * {
+ *    type: "/push/register",
+ *    data: {
+ *      ...
+ *    }
  * }
  *
  * If success, response status is 200
@@ -45,13 +51,16 @@ router.post('/register', function (req, res) {
   // check valid device_type
   if (!(body.device_type in DeviceType)) {res.status(400).send(getErrorResponse(20, 'device_type')); return;}
 
-  var message = {
-    device_name: body.device_name,
-    device_type: body.device_type,
-    device_id: body.device_id
+  var delivery = {
+    type: req.path,
+    data: {
+      device_name: body.device_name,
+      device_type: body.device_type,
+      device_id: body.device_id
+    }
   };
-  rabbitmq.push.write(JSON.stringify(message), 'utf8');
-  info('Write to MQ: ' + JSON.stringify(message));
+  rabbitmq.push.write(JSON.stringify(delivery), 'utf8');
+  info('Write to MQ: ' + JSON.stringify(delivery));
   res.status(200).end();
 });
 
@@ -69,7 +78,7 @@ router.post('/register', function (req, res) {
  * 20 : someone stars your thread
  * 21 : someone stars your comment
  *
- * Request
+ * Client Request
  * {
  *    push_type:    (required, Number),
  *    push_badge:   (optional, Number),
@@ -79,6 +88,14 @@ router.post('/register', function (req, res) {
  *    summary:      (required, String),
  *    thread_id:    (optional, Number),
  *    comment_id:   (optional, Number)
+ * }
+ *
+ * RabbitMQ Request
+ * {
+ *    type: "/push/message",
+ *    data: {
+ *      ...
+ *    }
  * }
  *
  * Response
@@ -108,21 +125,31 @@ router.post('/message', function (req, res) {
   body.thread_id = body.thread_id !== undefined ? body.thread_id : '';
   body.comment_id = body.comment_id !== undefined ? body.comment_id : '';
 
-  var message = {
-    push_type: body.push_type,
-    push_badge: body.push_badge,
-    push_message: body.push_message,
-    event_date: body.event_date,
-    image_url: body.image_url,
-    summary: body.summary,
-    thread_id: body.thread_id,
-    comment_id: body.comment_id
+  var delivery = {
+    type: req.path,
+    data: {
+      push_type: body.push_type,
+      push_badge: body.push_badge,
+      push_message: body.push_message,
+      event_date: body.event_date,
+      image_url: body.image_url,
+      summary: body.summary,
+      thread_id: body.thread_id,
+      comment_id: body.comment_id
+    }
   };
-  rabbitmq.push.write(JSON.stringify(message), 'utf8');
-  info('Write to MQ: ' + JSON.stringify(message));
+  rabbitmq.push.write(JSON.stringify(delivery), 'utf8');
+  info('Write to MQ: ' + JSON.stringify(delivery));
   res.status(200).end();
 });
 
+/** Make error response object
+ * @private
+ * @function
+ * @param errorCode
+ * @param param Used for additional error description
+ * @returns {{message: string, code: (Number)}}
+ */
 function getErrorResponse(errorCode, param) {
   var message = 'Response error code does not detected';
   param = param !== undefined ? param : ' ';
